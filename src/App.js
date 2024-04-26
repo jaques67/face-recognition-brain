55aa0e1ee9a2d1269d3e8c60dd286755ce3c7bd1
DESKTOP-7DB5H5Q\Jaques <jaques@cyberia.technology>
2024-04-26 13:29:37 +0200

    Added ImageLinkForm component. Added particles-bg for moving background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div>
       <Navigation />
-      <Logo/>
-      {/*  <ImageLinkForm />*/}
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
       {/*  <FaceRecognition />}*/}
+      <ParticlesBg type="cobweb" bg={true} />
     </div>
   );
 }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import brain from './brain.png';
 const Logo = () => {
   return (
     <div className="ma4 mt0 mw4">
-      <Tilt  className='br2 shadow-2' tiltMaxAngleX={35} tiltMaxAngleY={35} >
+      <Tilt  className='br2 shadow-2 Tilt' tiltMaxAngleX={35} tiltMaxAngleY={35} >
         <div>
           <div><img src={brain}  alt='brain'/></div>
         </div>
