d80809842ed676252fbe547973e48532edd30767
Jaques Strydom <jaques.strydom@gmail.com>
2024-04-27 08:20:23 +0200

    Converted App to class to use state. Added code for textbox and button submit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -5,17 +6,37 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
 
-function App() {
-  return (
-    <div>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*  <FaceRecognition />}*/}
-      <ParticlesBg type="cobweb" bg={true} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/*  <FaceRecognition />}*/}
+        <ParticlesBg type="cobweb" bg={true}/>
+      </div>
+    );
+  }
 }
 
 export default App;
