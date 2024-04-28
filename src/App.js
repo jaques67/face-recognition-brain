0e6e268c0593fdb684874341601b026544eba7de
Jaques Strydom <jaques.strydom@gmail.com>
2024-04-28 11:15:34 +0200

    Add face recognition via Clarifai API returning the bounding box coordinates. Also displays image below input box.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ParticlesBg from 'particles-bg'
 
 class App extends Component {
@@ -11,16 +12,70 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
+  returnClarifaiRequestOptions(imgUrl) {
+    const PAT = '';
+    const USER_ID = '';
+    const APP_ID = '';
+    // const MODEL_ID = 'face-detection';
+    const IMAGE_URL = imgUrl;
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+      },
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": IMAGE_URL
+            }
+          }
+        }
+      ]
+    });
+
+    return {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+  }
+
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
+    this.setState({imageUrl: this.state.input}, () => {
+      const requestOptions = this.returnClarifaiRequestOptions(this.state.imageUrl);
+      fetch(
+        "https://api.clarifai.com/v2/models/" +
+        'face-detection' +
+        "/outputs",
+        requestOptions
+      )
+        .then(response => response.json())
+        .then((response) => {
+            console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+            // do something with response
+          },
+        //   function (err) {
+        //     console.log('error: ', err)
+        //     // there was an error
+        //   }
+        )
+        .catch(err => console.log(err))
+    });
   }
+
 
   render() {
     return (
@@ -32,7 +87,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/*  <FaceRecognition />}*/}
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
         <ParticlesBg type="cobweb" bg={true}/>
       </div>
     );
